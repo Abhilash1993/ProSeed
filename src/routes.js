be167fca6616fb954d3ad1dab33d8723d28a45d9
Abhilash Kulkarni <abhikul1993@gmail.com>
2018-02-28 23:07:19 +0530

    Login UI completed
    Login UI completed

--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,14 @@ import reducers from './root_reducer';
 import { BrowserRouter, Switch, Route, Router } from 'react-router-dom';
 
 import Home from './home/Home';
-import About from './about/About';
+import Login from './login/login';
 import Dummy from './dummy/Dummy';
 
 const routes = (
     <BrowserRouter history={history}>
-      <Switch> 
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/login' component={Login} />
         <Route exact path='/dummy' component={Dummy} />
       </Switch>
     </BrowserRouter>
